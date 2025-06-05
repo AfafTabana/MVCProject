ada93e57af13b16eb5068868db8f3194e86af889
Eslam-Mohamed <eslamooo5522@outlook.com>
2025-06-05 03:37:36 +0300

    Book Repo Interface

---
+++ MVCProject/Repository/IBookRepository.cs
@@ -0,0 +1,20 @@
+﻿using MVCProject.Models;
+
+namespace MVCProject.Repository
+{
+    public interface IBookRepository
+    {
+      
+        IEnumerable<Books> GetAllBooks();
+        
+        Books GetBookById(int id);
+
+        Books GetBookByTitle(string title);
+
+        void AddBook(Books book);
+        
+        void UpdateBook(Books book);
+        
+        void DeleteBook(int id);
+    }
+}
